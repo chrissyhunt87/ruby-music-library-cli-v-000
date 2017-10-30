327cd7357f929780492768b43def6014670a4a52
Chrissy Hunt <chrissy.hunt@gmail.com>
2017-10-30 13:26:20 +0000

    automatically backed up by learn

--- lib/musiclibrarycontroller.rb
+++ lib/musiclibrarycontroller.rb
@@ -84,6 +84,7 @@ class MusicLibraryController
     list_songs
     puts "Which song number would you like to play?"
     song_number = gets.chomp
+    index = song_number.to_i - 1
     sorted_songs = Song.all.sort_by {|song| song.name}
     puts "#{song_number}. #{sorted_songs[song_number-1].artist.name} - #{sorted_songs[song_number-1].name} - #{sorted_songs[song_number-1].genre.name}"
   end
